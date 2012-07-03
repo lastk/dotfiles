5be74eaf5ed9a05346fd4710010976d1a2a75806
Rafael Oliveira <rafael.oliveira@gmail.com>
2012-07-02 21:07:03 -0300

    name in the same line of dir

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -72,9 +72,9 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'[%n@%m] \n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'[%n@%m] $(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
+  export RPROMPT="$%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
 
 precmd() {
